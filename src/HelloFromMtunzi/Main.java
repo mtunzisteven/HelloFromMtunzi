a51099ca2eb55731941a09328829fae2506b94f6
Scott Currell <59635303+currell-scott@users.noreply.github.com>
2020-02-04 08:20:29 -0700

    Update Main.java

--- src/HelloFromMtunzi/Main.java
+++ src/HelloFromMtunzi/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Mtunzi");
+        System.out.println("Hello, Mtunzi. My name is Scott.");
     }
 }
